36e6291150ae1175021cd223e1f661a386634e27
nronzel <86695181+nronzel@users.noreply.github.com>
2023-06-01 20:21:36 -0400

    fix: unique key
    unique key was capitalized when the bson data is lowercase. Data now
    gets saved properly to the db.

--- db.go
+++ db.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-    "log"
+	"log"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -14,54 +14,63 @@ import (
 
 func connectMongo() *mongo.Collection {
 
-	uri := "mongodb://127.0.0.1/27017"
+    uri := "mongodb://127.0.0.1:27017"
 
 	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(uri))
 
 	if err != nil {
-        log.Println("Error connecting to DB", err)
+		log.Println("Error connecting to DB", err)
 	}
 
 	// Ping Mongo
 	err = client.Ping(context.Background(), nil)
 
 	if err != nil {
-        log.Printf("Error pinging database: %v\n", err)
+		log.Printf("Error pinging database: %v\n", err)
 	}
 
 	fmt.Println("Connected to MongoDB")
 
 	collection := client.Database("realtor").Collection("houses")
 
-    // Create unique index to prevent duplicates
+	// Create unique index to prevent duplicates
 	indexModel := mongo.IndexModel{
 		Keys: bson.M{
-			"Link": 1,
+			"link": 1,
 		},
 		Options: options.Index().SetUnique(true),
 	}
 
 	_, err = collection.Indexes().CreateOne(context.Background(), indexModel)
 	if err != nil {
-        log.Printf("Error: %v", err)
+		log.Printf("Error: %v", err)
 	}
 
 	return collection
 }
 
 func insertMongo(collection *mongo.Collection) {
-    // Read data from master.json
-    data, err := ioutil.ReadFile("master.json")
+	// Read data from master.json
+	data, err := ioutil.ReadFile("master.json")
 
-    if err != nil {
-        log.Printf("Error: %v\n", err)
-    }
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
 
-    var houses []house
+	var houses []house
 
-    err = json.Unmarshal(data, &houses)
-    if err != nil {
-        log.Printf("Error umarshaling json: %v\n", err)
-    }
+	err = json.Unmarshal(data, &houses)
+	if err != nil {
+		log.Printf("Error umarshaling json: %v\n", err)
+	}
 
+	housesInterface := make([]interface{}, len(houses))
+	for i, v := range houses {
+		housesInterface[i] = v
+	}
+
+	_, err = collection.InsertMany(context.Background(), housesInterface)
+	if err != nil {
+		log.Printf("Error inserting data into collection: %v", err)
+	}
 }
