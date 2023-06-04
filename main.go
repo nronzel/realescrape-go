8d169db528b8ffe229816a4dc2468028a5032ab2
nronzel <86695181+nronzel@users.noreply.github.com>
2023-06-04 19:34:18 -0400

    feat: created simple API to get all houses
    API has a single GET endpoint that gets all houses from the MongoDB
    collection
    
    Also added check in main() for the command line argument.

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strings"
 
+	"github.com/nronzel/realescrape-go/api"
 	"github.com/nronzel/realescrape-go/db"
 	"github.com/nronzel/realescrape-go/scraper"
 )
@@ -12,7 +14,15 @@ func main() {
     // Connect to collection
 	collection := db.ConnectMongo()
 
+    if len(os.Args) < 2 {
+        fmt.Println("Please provide a location as an argument.")
+        os.Exit(1)
+    }
+
 	location := strings.ReplaceAll(os.Args[1], " ", "_")
 
 	scraper.RunScraper(collection, location)
+
+    // Start the Echo API on localhost:3000
+    api.StartAPI(collection)
 }
