d8b6b5d652ccf3bcb04a4d18f010eeff3cf7a094
nronzel <86695181+nronzel@users.noreply.github.com>
2023-06-04 20:47:17 -0400

    fixed indentation

--- api/api.go
+++ api/api.go
@@ -14,68 +14,68 @@ import (
 
 func getAllHouses(collection *mongo.Collection) echo.HandlerFunc {
 	return func(c echo.Context) error {
-        // Create context with timeout
+		// Create context with timeout
 		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 
-        // Call the cancel function to avoid context leak
+		// Call the cancel function to avoid context leak
 		defer cancel()
 
-        // Attempts to find all documents in MongoDB collection
+		// Attempts to find all documents in MongoDB collection
 		cursor, err := collection.Find(ctx, bson.M{})
 		if err != nil {
-            // Log the error
-            log.Printf("Failed to find houses: %v", err)
-            // Return generic error message to client with 500 status
+			// Log the error
+			log.Printf("Failed to find houses: %v", err)
+			// Return generic error message to client with 500 status
 			return c.JSON(http.StatusInternalServerError, "Failed to find houses")
 		}
 
-        // Close the cursor if no errors found when done
+		// Close the cursor if no errors found when done
 		defer cursor.Close(ctx)
 
 		houses := []models.House{}
 
-        // Iterate over cursor, decoding each doc into House struct
+		// Iterate over cursor, decoding each doc into House struct
 		for cursor.Next(ctx) {
 			var house models.House
 			err := cursor.Decode(&house)
 			if err != nil {
-                // Log the error
-                log.Printf("Failed to decode house: %v", err)
-                // Return a generic error message to client with 500 status
+				// Log the error
+				log.Printf("Failed to decode house: %v", err)
+				// Return a generic error message to client with 500 status
 				return c.JSON(http.StatusInternalServerError, "Failed to decode house")
 			}
 
-            // Append the house to our slice if no errors
+			// Append the house to our slice if no errors
 			houses = append(houses, house)
 		}
 
-        // Check for remaining cursor errors after loop
+		// Check for remaining cursor errors after loop
 		if err := cursor.Err(); err != nil {
-            log.Printf("Cursor error: %v", err)
+			log.Printf("Cursor error: %v", err)
 			return c.JSON(http.StatusInternalServerError, "Failed to retrieve houses")
 		}
 
-        // Send 200 status code with the response
+		// Send 200 status code with the response
 		err = c.JSON(http.StatusOK, houses)
-        if err != nil {
-            log.Printf("Failed to send response: %v", err)
-        }
+		if err != nil {
+			log.Printf("Failed to send response: %v", err)
+		}
 
-        return err
+		return err
 	}
 }
 
 func StartAPI(collection *mongo.Collection) {
-    // Create new Echo app
+	// Create new Echo app
 	e := echo.New()
 
-    // Register getAllHouses handler to /houses endpoint
-    // Responds to GET requests with all items in the MongoDB collection
+	// Register getAllHouses handler to /houses endpoint
+	// Responds to GET requests with all items in the MongoDB collection
 	e.GET("/houses", getAllHouses(collection))
 
-    // Start server on port localhost:6000
-    err := e.Start(":3000")
-    if err != nil {
-        log.Fatalf("Failed to start the server: %v", err)
-    }
+	// Start server on port localhost:6000
+	err := e.Start(":3000")
+	if err != nil {
+		log.Fatalf("Failed to start the server: %v", err)
+	}
 }
