875e6ef9506a2ab626132f112959848d2a825e57
nronzel <86695181+nronzel@users.noreply.github.com>
2023-06-05 20:34:32 -0400

    separated handler and endpoint defs

---
+++ api/getHandler.go
@@ -0,0 +1,66 @@
+package api
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/nronzel/realescrape-go/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func getAllHouses(collection *mongo.Collection) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		// Create context with timeout
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+		// Call the cancel function to avoid context leak
+		defer cancel()
+
+		// Attempts to find all documents in MongoDB collection
+		cursor, err := collection.Find(ctx, bson.M{})
+		if err != nil {
+			// Log the error
+			log.Printf("Failed to find houses: %v", err)
+			// Return generic error message to client with 500 status
+			return c.JSON(http.StatusInternalServerError, "Failed to find houses")
+		}
+
+		// Close the cursor if no errors found when done
+		defer cursor.Close(ctx)
+
+		houses := []models.House{}
+
+		// Iterate over cursor, decoding each doc into House struct
+		for cursor.Next(ctx) {
+			var house models.House
+			err := cursor.Decode(&house)
+			if err != nil {
+				// Log the error
+				log.Printf("Failed to decode house: %v", err)
+				// Return a generic error message to client with 500 status
+				return c.JSON(http.StatusInternalServerError, "Failed to decode house")
+			}
+
+			// Append the house to our slice if no errors
+			houses = append(houses, house)
+		}
+
+		// Check for remaining cursor errors after loop
+		if err := cursor.Err(); err != nil {
+			log.Printf("Cursor error: %v", err)
+			return c.JSON(http.StatusInternalServerError, "Failed to retrieve houses")
+		}
+
+		// Send 200 status code with the response
+		err = c.JSON(http.StatusOK, houses)
+		if err != nil {
+			log.Printf("Failed to send response: %v", err)
+		}
+
+		return err
+	}
+}
