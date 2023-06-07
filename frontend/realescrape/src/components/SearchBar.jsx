35a85e6d1f90ac5c4e472e71548d236dd966ffdd
nronzel <86695181+nronzel@users.noreply.github.com>
2023-06-07 11:35:50 -0400

    feat: searchbar allows search and scrapes data

--- frontend/realescrape/src/components/SearchBar.jsx
+++ frontend/realescrape/src/components/SearchBar.jsx
@@ -1,4 +1,27 @@
-const SearchBar = () => {
+import { createSignal } from "solid-js";
+
+const SearchBar = (props) => {
+  const [location, setLocation] = createSignal("");
+
+  const searchLocation = async () => {
+    try {
+      const encodedLocation = encodeURIComponent(location());
+      const url = `http://localhost:3000/scrape/${encodedLocation}`;
+      const response = await fetch(url, { method: "POST" });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+
+      props.onSearch(true);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <div className="flex justify-center items-center gap-4">
       <div className="form-control w-full max-w-xs">
@@ -9,6 +32,7 @@ const SearchBar = () => {
           type="text"
           placeholder=""
           className="input input-bordered w-full max-w-xs outline-none"
+          onInput={(e) => setLocation(e.target.value)}
         />
         <label className="label">
           <span className="label-text-alt">
@@ -16,7 +40,12 @@ const SearchBar = () => {
           </span>
         </label>
       </div>
-      <input type="button" value="Search" className="btn btn-primary btn-sm" />
+      <input
+        type="button"
+        value="Search"
+        className="btn btn-primary btn-sm"
+        onClick={searchLocation}
+      />
     </div>
   );
 };
