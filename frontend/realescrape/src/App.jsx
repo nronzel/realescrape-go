476c0280c7553742ea1785153ae61fdb0ddb1b72
nronzel <86695181+nronzel@users.noreply.github.com>
2023-06-07 11:34:59 -0400

    search signal to re-fetch

--- frontend/realescrape/src/App.jsx
+++ frontend/realescrape/src/App.jsx
@@ -1,13 +1,17 @@
 import Header from "./components/Header.jsx";
 import SearchBar from "./components/SearchBar.jsx";
 import DataTable from "./components/DataTable.jsx";
+import { createSignal } from "solid-js";
 
 function App() {
+  const [searchPerformed, setSearchPerformed] = createSignal(false);
+  const [searchQuery, setSearchQuery] = createSignal("");
+
   return (
     <div className="main">
       <Header />
-      <SearchBar />
-      <DataTable />
+      <SearchBar onSearch={setSearchPerformed} onQuery={setSearchQuery} />
+      <DataTable searchPerformed={searchPerformed} searchQuery={searchQuery} />
     </div>
   );
 }
