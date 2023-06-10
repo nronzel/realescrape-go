55650b029d360d9d46584961f4cc91d5dcca3140
nronzel <86695181+nronzel@users.noreply.github.com>
2023-06-09 23:14:33 -0400

    removed unused code

--- main.go
+++ main.go
@@ -14,15 +14,6 @@ func main() {
 		fmt.Println("Error: ", err)
 	}
 
-	// if len(os.Args) < 2 {
-	// 	fmt.Println("Please provide a location as an argument.")
-	// 	os.Exit(1)
-	// }
-	//
-	// location := strings.ReplaceAll(os.Args[1], " ", "_")
-	//
-	// scraper.RunScraper(collection, location)
-
 	// Start the Echo API on localhost:3000
 	api.StartAPI(collection)
 }
