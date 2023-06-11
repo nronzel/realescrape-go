d55478497aba7f022d66bf24aa071b9633f388e5
nronzel <86695181+nronzel@users.noreply.github.com>
2023-06-10 21:30:33 -0400

    refactored to use the event bus

--- api/sse.go
+++ api/sse.go
@@ -1,42 +1,30 @@
 package api
 
 import (
-	"context"
 	"fmt"
 	"time"
 
 	"github.com/labstack/echo/v4"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func sseHandler(c echo.Context, collection *mongo.Collection, e *echo.Echo) error {
+func sseHandler(c echo.Context, e *echo.Echo, eb *EventBus) error {
 	e.Logger.Info("SSE Handler called")
 	c.Response().Header().Set(echo.HeaderContentType, "text/event-stream")
 	c.Response().Header().Set(echo.HeaderConnection, "keep-alive")
 	c.Response().Header().Set("Cache-Control", "no-cache")
 	c.Response().Header().Set("Access-Control-Allow-Origin", "*")
 
-	prevCount := int64(0)
+	eventChannel := eb.Subscribe()
+
 	for {
-		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-		defer cancel()
-
-		currCount, err := collection.CountDocuments(ctx, bson.M{})
-		if err != nil {
-			if cmdErr, ok := err.(mongo.CommandError); ok {
-				e.Logger.Error(fmt.Sprintf("Error counting documents: %v. Code: %v, Name: %v", cmdErr.Message, cmdErr.Code, cmdErr.Name))
-			} else {
-				e.Logger.Error("Error counting documents:", err)
+		select {
+		case currEvent, ok := <-eventChannel:
+			if !ok {
+				// If the channel is closed, break the loop
+				break
 			}
-			time.Sleep(time.Second * 2)
-			continue
-		}
 
-		if prevCount != currCount {
-			_, err := fmt.Fprintf(c.Response(), "data: %d\n\n", currCount)
-			e.Logger.Info(fmt.Sprintf("Updated Count: %v", currCount))
-
+			_, err := fmt.Fprintf(c.Response(), "data: %s\n\n", currEvent)
 			if err != nil {
 				e.Logger.Error(fmt.Sprintf("Error writing to response: %v", err))
 				// This likely means that the client has disconnected.
@@ -44,12 +32,9 @@ func sseHandler(c echo.Context, collection *mongo.Collection, e *echo.Echo) erro
 			}
 
 			c.Response().Flush()
-			prevCount = currCount
+		case <-time.After(500 * time.Second):
+			// Close the connection if no message is received in above time
+			return nil
 		}
-
-		time.Sleep(time.Second * 2)
 	}
-
-	return nil
-
 }
