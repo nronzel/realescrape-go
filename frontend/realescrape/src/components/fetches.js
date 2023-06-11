a53b902e73281aeae9a3f3e853116511e0212ae9
nronzel <86695181+nronzel@users.noreply.github.com>
2023-06-10 22:32:20 -0400

    refactored fetch functions

--- frontend/realescrape/src/components/fetches.js
+++ frontend/realescrape/src/components/fetches.js
@@ -1,22 +1,30 @@
 export async function getCount() {
   const res = await fetch("http://localhost:3000/houses/count");
-  const json = await res.json();
+  const count = await res.json();
 
-  return json.count;
+  return count.count;
 }
 
-export async function getData(currentPage) {
-  const url = `http://localhost:3000/houses?page=${currentPage}&limit=20`;
+export async function getData(currentPage, limit = 20) {
+  const url = `http://localhost:3000/houses?page=${currentPage}&limit=${limit}`;
   const res = await fetch(url);
-  const json = await res.json();
+  const data = await res.json();
 
-  return json;
+  return data;
+}
+
+export async function getAllData() {
+  const url = "http://localhost:3000/houses";
+  const res = await fetch(url);
+  const data = await res.json();
+
+  return data;
 }
 
 export async function deleteData() {
-    const url = "http://localhost:3000/cleardb";
-    const res = await fetch(url, { method: "POST" });
-    const json = await res.json();
+  const url = "http://localhost:3000/cleardb";
+  const res = await fetch(url, { method: "POST" });
+  const json = await res.json();
 
-    console.log(json)
+  console.log(json);
 }
