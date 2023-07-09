a2672ecf6cb0c0cf71714cced031e356900a303d
nronzel <86695181+nronzel@users.noreply.github.com>
2023-07-09 11:41:51 -0400

    fixed json formatting

--- models/models.go
+++ models/models.go
@@ -3,19 +3,19 @@ package models
 import "time"
 
 type House struct {
-	Price     int       `json:"Price" bson:"price"`
-	Beds      int       `json:"Beds" bson:"beds"`
-	Baths     float64   `json:"Baths" bson:"baths"`
-	Sqft      int       `json:"Sqft" bson:"sqft"`
-	LotSize   float64   `json:"LotSize" bson:"lotSize"`
-	LotUnit   string    `json:"LotUnit" bson:"lotUnit"`
-	LotSqft   int       `json:"LotSqft" bson:"lotSqft"`
-	Hty       float64   `json:"Hty" bson:"hty"`
-	HtyPcnt   float64   `json:"HtyPcnt" bson:"htyPcnt"`
-	Street    string    `json:"Street" bson:"street"`
-	City      string    `json:"City" bson:"city"`
-	State     string    `json:"State" bson:"state"`
-	Zip       string    `json:"Zip" bson:"zip"`
-	Link      string    `json:"Link" bson:"link"`
-	CrawlTime time.Time `json:"CrawlTime" bson:"crawlTime"`
+	Price     int       `json:"price" bson:"price"`
+	Beds      int       `json:"beds" bson:"beds"`
+	Baths     float64   `json:"baths" bson:"baths"`
+	Sqft      int       `json:"sqft" bson:"sqft"`
+	LotSize   float64   `json:"lotSize" bson:"lotSize"`
+	LotUnit   string    `json:"lotUnit" bson:"lotUnit"`
+	LotSqft   int       `json:"lotSqft" bson:"lotSqft"`
+	Hty       float64   `json:"hty" bson:"hty"`
+	HtyPcnt   float64   `json:"htyPcnt" bson:"htyPcnt"`
+	Street    string    `json:"street" bson:"street"`
+	City      string    `json:"city" bson:"city"`
+	State     string    `json:"state" bson:"state"`
+	Zip       string    `json:"zip" bson:"zip"`
+	Link      string    `json:"link" bson:"link"`
+	CrawlTime time.Time `json:"crawlTime" bson:"crawlTime"`
 }
